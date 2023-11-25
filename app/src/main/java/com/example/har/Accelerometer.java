e64750ced3d0b81553d219222a5f303a4a615cb8
Usman <alie25708@gmail.com>
2023-11-25 15:12:19 +0500

    Data Entry done

--- app/src/main/java/com/example/har/Accelerometer.java
+++ app/src/main/java/com/example/har/Accelerometer.java
@@ -10,12 +10,15 @@ import android.widget.Toast;
 public class Accelerometer implements SensorEventListener
 {
     TextView textView;
+    MainActivity mainActivity;
+    float x,y,z;
+    int i = 0;
     Sensor accelerometer;
-    Accelerometer(SensorManager mgr, TextView textView)
+    Accelerometer(SensorManager mgr, TextView textView, MainActivity mainActivity)
     {
         this.textView = textView;
         this.accelerometer = mgr.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-
+        this.mainActivity = mainActivity;
         // check is sensor is working or not
 
         if(accelerometer!=null)
@@ -36,7 +39,16 @@ public class Accelerometer implements SensorEventListener
     public void onSensorChanged(SensorEvent event) {
         if(event.sensor.getType()== Sensor.TYPE_ACCELEROMETER)
         {
+            x = event.values[0];
+            y = event.values[1];
+            z = event.values[2];
             textView.setText("Accelerometer vals:\n X: "+event.values[0]+"\nY: "+event.values[1]+"\nZ: "+event.values[2]);
+            i++;
+            if(i == 500){
+                i = 0;
+                DataCollection dc = new DataCollection(mainActivity);
+                dc.updateSheet(x,y,z,"Accelerometer");
+            }
         }
         else {
             System.out.println("Sensor type is not accelerometer");
--- app/src/main/java/com/example/har/MainActivity.java
+++ app/src/main/java/com/example/har/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.har;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.SensorListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -9,7 +10,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     TextView textViewA, textViewG,textViewM;
-
+    SensorManager sensormgr;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,18 +23,19 @@ public class MainActivity extends AppCompatActivity {
 
 
         // sensor code ahead
-        SensorManager sensormgr = (SensorManager) getSystemService(SENSOR_SERVICE);
+         sensormgr = (SensorManager) getSystemService(SENSOR_SERVICE);
 
         //taking data from all sensors
 
         if (sensormgr != null) {
-            Accelerometer accelerometer = new Accelerometer(sensormgr, textViewA);
-            Gyroscope  gyroscope = new Gyroscope(sensormgr, textViewG);
-            Magnetometer magnet = new Magnetometer(sensormgr, textViewM);
+            Accelerometer accelerometer = new Accelerometer(sensormgr, textViewA, this);
+            Gyroscope  gyroscope = new Gyroscope(sensormgr, textViewG, this);
+            Magnetometer magnet = new Magnetometer(sensormgr, textViewM, this);
         } else {
             System.out.println("Sensor Manager is null");
             Toast.makeText(this, "Sensor mgr is null", Toast.LENGTH_SHORT).show();
         }
     }
+
 
 }
