f497190d24c20fb2687795b3acdedd3e8c3b3b5d
kashafashraf230 <kashafashraf412@gmail.com>
2024-01-21 19:24:24 +0500

    Unknown changes

--- app/src/main/java/com/example/har/MainActivity.java
+++ app/src/main/java/com/example/har/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.har;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothDevice;
+import android.content.IntentFilter;
 import android.hardware.SensorListener;
 import android.hardware.SensorManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -11,6 +15,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
@@ -19,18 +27,33 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.HttpURLConnection;
+import java.net.Socket;
+import java.net.URI;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.os.Bundle;
 
+
+import sensor.WatchSensor;
 import sensor.PhoneSensor;
+import sensor.WatchSensor;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView textViewA, textViewG,textViewM, IP;
+    TextView textViewA, textViewG,textViewM, IP, textConn;
     EditText IPtext;
 
-    Button btnIP, btndis;
+    Button btnIP, btndis, search;
+    private Socket client;
+    private PrintWriter printwriter;
+    BluetoothAdapter bt;
 
+    private static final int REQUEST_ENABLE_BT = 1;
 
     SensorManager sensormgr;
     @Override
@@ -39,9 +62,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // bind views
-        textViewA = (TextView) findViewById(R.id.acceleroView);
-      //  textViewG = (TextView) findViewById(R.id.gyroView);
-        //textViewM = (TextView) findViewById(R.id.magnetoView);
+
+        textConn = findViewById(R.id.connection);
 
         //binding Server IPs views and button
         IP = findViewById(R.id.ServerIp);
@@ -54,44 +76,75 @@ public class MainActivity extends AppCompatActivity {
          sensormgr = (SensorManager) getSystemService(SENSOR_SERVICE);
 
         //taking data from all sensors
-/*
+        ArrayList<List<Double>> sensorDataList= null;
+
         if (sensormgr != null) {
-            Accelerometer accelerometer = new Accelerometer(sensormgr, textViewA, this);
-            Gyroscope  gyroscope = new Gyroscope(sensormgr, textViewG, this);
-            Magnetometer magnet = new Magnetometer(sensormgr, textViewM, this);
+            //class to get data from the sensors
+            PhoneSensor sensors = new PhoneSensor(sensormgr);
+            //this function returns data of all sensors in a list of lists
+            sensorDataList = sensors.get_all_data();
+
         } else {
             System.out.println("Sensor Manager is null");
             Toast.makeText(this, "Sensor mgr is null", Toast.LENGTH_SHORT).show();
         }
-*/
 
-        if(sensormgr!=null)
-        {
-            PhoneSensor sensors = new PhoneSensor(sensormgr, textViewA);
-        }
-        else {
-            System.out.println("Sensor Manager is null");
-            Toast.makeText(this, "Sensor mgr is null", Toast.LENGTH_SHORT).show();
-        }
-        final HttpURLConnection[] urlConnection = new HttpURLConnection[1];
+
 
         //connecting server and disconnecting on button clicks
         //creating server object and passing context to it so it can be used to create toasts
+        //sensor data is also passed to write it on server
 
         Server srvr = new Server(this);
-        srvr.connect_disconnect(btnIP, IPtext, urlConnection, btndis);
+        srvr.connect_disconnect(btnIP, IPtext, btndis, sensorDataList, textConn);
         //this method will take both buttons of connecting and disconnecting, so server can be connected accordingly
-        //url connection is also passed and also the IP of the server (IPtext)
+        //the ip of the server is passed to it as well
 
+
+
+
+        //bluetooth connections
+        search = findViewById(R.id.search);
+        //if search for device button is pressed then:
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                search.setText("Searching...");
+
+                //creating bluetooth adapter
+                bt = BluetoothAdapter.getDefaultAdapter();
+                if(bt == null || !bt.isEnabled())
+                {
+                    // Prompt user to enable Bluetooth
+                    try {
+                        Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                        startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+                    }
+                    catch (SecurityException e){
+                        //catch exception
+                    }
+
+                }
+                //Watch sensor class object which on receceiving a device will connect to it
+                WatchSensor receiver = new WatchSensor(bt);
+                IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+
+                registerReceiver(receiver, filter);
+
+                // Start discovery
+                try {
+                    bt.startDiscovery();
+                }
+                catch (SecurityException e){
+                    Log.d("error", "security exception");
+                }
+                //SmartwatchSensorData sensorData = smartwatchSdk.getSensorData();
+            }
+        });
 
     }
 
 
-
-
-
-
-
-
-
 }
+
+
--- app/src/main/java/sensor/Magnetometer.java
+++ app/src/main/java/sensor/Magnetometer.java
@@ -11,23 +11,23 @@ import com.example.har.DataCollection;
 import com.example.har.MainActivity;
 
 public class Magnetometer implements SensorEventListener {
-    TextView textView;
+    //TextView textView;
     MainActivity mainActivity;
     float x,y,z;
     int i = 0;
     Sensor magnetometer;
-    Magnetometer(SensorManager mgr, TextView textView)
+    Magnetometer(SensorManager mgr)
     {
-        this.textView = textView;
+        //this.textView = textView;
         this.magnetometer = mgr.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
-       // this.mainActivity  = mainActivity;
+        // this.mainActivity  = mainActivity;
         if(magnetometer!=null)
         {
             mgr.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_NORMAL);
         }
         else {
 
-            Toast.makeText(textView.getContext(), "Magnetometer not found",Toast.LENGTH_SHORT).show();
+            //Toast.makeText(textView.getContext(), "Magnetometer not found",Toast.LENGTH_SHORT).show();
             System.out.println("Magnetometer not found");
         }
     }
@@ -47,7 +47,7 @@ public class Magnetometer implements SensorEventListener {
             x = event.values[0];
             y = event.values[1];
             z = event.values[2];
-            textView.setText("Magnetometer vals:\n X: "+event.values[0]+"\nY: "+event.values[1]+"\nZ: "+event.values[2]);
+            //textView.setText("Magnetometer vals:\n X: "+event.values[0]+"\nY: "+event.values[1]+"\nZ: "+event.values[2]);
         }
 
     }
